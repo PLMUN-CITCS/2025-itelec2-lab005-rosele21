c5792dd7efebb16874e71291e9dad935b55577d8
rosele21 <>
2025-03-01 15:52:29 +0800

    Last Sync: 2025-03-01 07:52:29 (Mobile)

---
+++ exercise_03.py
@@ -0,0 +1,19 @@
+a = 10
+b = 5
+c = 3
+result1 = a + b * c
+print("Result of a + b * c:", result1)
+result2 = (a + b) * c
+print("Result of (a + b) * c:", result2)
+result3 = a - b
+print("Result of a - b:", result3)
+result4 = a / b
+result5 = a // b
+print("Result of a / b:", result4)
+print("Result of a // b (floor division):", result5)
+result6 = a % b
+result7 = a ** c
+print("Result of a % b (modulus):", result6)
+print("Result of a ** c (exponentiation):", result7)
+result8 = (a + b - c) * (a / b)
+print("Result of (a + b - c) * (a / b):", result8)
